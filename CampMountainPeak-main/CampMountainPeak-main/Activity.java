2f03cfa198b788d71889635cdc1025ad7b00a161
Archimedes-C <89412917+Archimedes-C@users.noreply.github.com>
2024-05-17 18:22:46 -0400

    Delete CampMountainPeak-main/CampMountainPeak-main directory
    Removed unnecessary files

--- CampMountainPeak-main/CampMountainPeak-main/Activity.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * @author Andrew Chisolm, Bryan Munoz, Matt Olson, Daniel Ruiz, Jaden Arnold
- * Different types of Activities offered
- */
-public enum Activity {
-    KAYAKING, ROCK_CLIMBING, VOLLEYBALL, ARCHERY, SWIMMING, HIKING, BACKPACKING, CYCLING, YOGA, PADDLEBOARDING, CHOPPING_WOOD, ROPES_COURSE, BOULDERING, BONFIRE, HUNTING
-}
